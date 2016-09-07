c7eeda152a76ba0f854e86f56bc0d25476df5839
Adam Furmanek <adam.furmanek@smtsoftware.com>
2016-09-07 17:28:05 +0200

    First version.

---
+++ README.md
@@ -0,0 +1,8 @@
+# MilpOperatorsWrapper
+Operators wrapper for MilpManager variables.
+
+# What is it
+This library extends MilpManager https://github.com/afish/MilpManager with ordinary C# operators for IVariable.
+
+# How to use it
+Add package to project and you are good to go.
---
+++ VariableWrapperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MilpManager.Abstraction;
+
+namespace MilpOperatorsWrapper
+{
+    public static class VariableWrapperExtensions
+    {
+        /// <summary>
+        /// Wraps variable into wrapper with overloaded operators
+        /// </summary>
+        /// <param name="variable">Variable to wrap</param>
+        /// <returns>Wrapped variable</returns>
+        public static VariableWrapper Wrap(this IVariable variable)
+        {
+            return new VariableWrapper(variable);
+        }
+    }
+}
